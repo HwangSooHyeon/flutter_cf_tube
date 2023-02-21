9a078a01a02cdb9a7088d775c5259cce33d22f01
amb <mikaelh1651@gmail.com>
2023-02-21 18:08:56 +0900

    feat(api): 유튜브 api 값 저장

---
+++ lib/const/api.dart
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyA-3siZu09hXawyBuUwTpZlqGGP6LYfrCI';
+const YOUTUBE_API_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/search';
+const CF_CHANNEL_ID = 'UCxZ2AlaT0hOmxzZVbF_j_Sw';
