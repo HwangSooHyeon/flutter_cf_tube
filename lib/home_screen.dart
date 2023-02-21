d8a219c45b1449d31d4568a11c4d728f9da3fc57
amb <mikaelh1651@gmail.com>
2023-02-21 17:56:30 +0900

    feat(custom_youtube_player): 유튜브 플레이어 작성 및 테스트

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cf_tube/component/custom_youtube_player.dart';
+import 'package:flutter_cf_tube/model/video_model.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: CustomYoutubePlayer(
+        videoModel: VideoModel(
+          id: '3Ck42C2ZCb8',
+          title: '다트 언어 기본기 1시간만에 끝내기',
+        ),
+      ),
+    );
   }
 }
